a75737475b4ff21cb75d1628d2a6b97d1d6cf81b
GHackAnonymous <ederaraia@gmail.com>
2017-09-26 17:24:06 +0200

    Ejercicio productor consumidor con semaforos

--- Ejercicios/EjercicioProductorConsumidor/main.c
+++ Ejercicios/EjercicioProductorConsumidor/main.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 int valor[2];
+int cnt;
 
 sem_t sMe,sC,sP;
 
 void *rutinaProductor(void *arg)
 {
-    sem_wait(&sMe);
+    //while(1)
+    for(int i = 0; i<10;i++)
+    {
+        sem_wait(&sP);
+        sem_wait(&sMe);
+        //seccion critica
+
+
+        if(cnt < 2 ){
+
+            valor[cnt] = rand();
+
+            printf("El hilo %d a metido el numero es: %d\n",(int)arg,valor[cnt]);
+            printf("CNT: %d\n",cnt);
+
+            cnt++;
+            printf("CNT: %d\n",cnt);
+        }
+        sem_post(&sMe);
+        sem_post(&sC);
+    }
+    printf("Salgo rutina hilo %d\n",(int)arg);
 }
 void *rutinaConsumidor(void *arg)
 {
-    for(int i = 0; i<30; i++)
+    //while(1)
+    for(int i = 0; i<20;i++)
     {
-        printf("Hola %d del hilo%d\n",i, (int*) arg);
-        sleep(1);
+
+        printf("Valor de i: %d\n",i);
+
+        sem_wait(&sC);
+        sem_wait(&sMe);
+        //seccion critica
+
+
+        if(cnt <= 2 ){
+
+            printf("                         El hilo %d a sacado el numero es: %d\n",(int)arg,valor[cnt]);
+            printf("                         CNT: %d\n",cnt);
+
+            cnt--;
+            printf("                         CNT: %d\n",cnt);
+       }
+        sem_post(&sMe);
+        sem_post(&sP);
     }
-    valor = 2;
+    printf("                                 Salgo rutina hilo %d\n",(int)arg);
 }
 
 int main()
@@ -25,13 +65,15 @@ int main()
 
     pthread_t hP1, hP2, hC1;
 
-    int sem_init(&sMe, 0, 1);
-    int sem_init(&sC, 0, 1);
-    int sem_init(&sP, 0, 2);
+    srand(10);
 
-    pthread_create(&hP1,NULL,rutina,(void*) "hiloProductor1");
-    pthread_create(&hP2,NULL,rutina2,(void*) "hiloProductor2");
-    pthread_create(&hC2,NULL,rutina2,(void*) "hiloConsumidor1");
+    sem_init(&sMe, 0, 1);
+    sem_init(&sC, 0, 0);
+    sem_init(&sP, 0, 2);
+
+    pthread_create(&hP1,NULL,rutinaProductor,(void*) 1);
+    pthread_create(&hP2,NULL,rutinaProductor,(void*) 2);
+    pthread_create(&hC1,NULL,rutinaConsumidor,(void*) 3);
 
     pthread_join(hP1, NULL);
     pthread_join(hP2, NULL);
